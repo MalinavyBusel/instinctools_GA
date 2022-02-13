2f8868ec50d12e8feee83f190d68e4fc1e31756d
Egor <egorbgtrvch@gmail.com>
2022-02-13 21:41:11 +0300

    Removed eval and enum from days.py.

--- Problems/days.py
+++ Problems/days.py
@@ -1,49 +1,41 @@
 from enum import Enum
 
-
-class Days(Enum):
-    понедельник = 5
-    пн = 5
-    monday = 5
-    вторник = 4
-    вт = 4
-    tuesday = 4
-    среда = 3
-    ср = 3
-    wednesday = 3
-    четверг = 2
-    чт = 2
-    thursday = 2
-    пятница = 1
-    пт = 1
-    friday = 1
-    суббота = 1
-    сб = 1
-    saturday = 1
-    воскресенье = 6
-    вс = 6
-    sunday = 6
+my_days = {'понедельник': 5,
+           'пн': 5,
+           'monday': 5,
+           'вторник': 4,
+           'вт': 4,
+           'tuesday': 4,
+           'среда': 3,
+           'ср': 3,
+           'wednesday': 3,
+           'четверг': 2,
+           'чт': 2,
+           'thursday': 2,
+           'пятница': 1,
+           'пт': 1,
+           'friday': 1,
+           'суббота': 1,
+           'сб': 1,
+           'saturday': 1,
+           'воскресенье': 6,
+           'вс': 6,
+           'sunday': 6}
 
 
-def days():
-    accepted = {'sunday', 'вс', 'воскресенье', 'saturday', 'сб', 'суббота', 'friday', 'пт', 'пятница', 'чт',
-                'четверг', 'ср', 'среда', 'вт', 'вторник', 'пн',
-                'понедельник', 'monday', 'tuesday', 'wednesday', 'thursday'}
+def days(my_days: dict):
     day = input('Введите день недели: ').lower()
 
-    # При надобности могу сделать без eval, но тогда, наверное, придется убрать Enum
-    if day in accepted:
-        to_wait = eval(f'Days.{day}.value')
-    else:
-        print('Введите день корректно!')
-        return days()
+    to_wait = my_days.setdefault(day, 'smth')
     if to_wait == 1:
         return 'Вам осталось ждать 1 день.'
     elif to_wait in [2, 3, 4, ]:
         return f'Вам осталось ждать {to_wait} дня.'
     elif to_wait in [5, 6, ]:
         return f'Вам осталось ждать {to_wait} дней.'
+    else:
+        return 'No such day'
 
 
 if __name__ == '__main__':
-    print(days())
+    print(days(my_days))
