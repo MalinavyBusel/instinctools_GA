f493d53755139c6be431acac370751a52431d22f
Egor <egorbgtrvch@gmail.com>
2022-02-22 12:08:04 +0300

    added a one-liner of a loop as a comment, changed dangerous name

--- week2/Monte-Carlo.py
+++ week2/Monte-Carlo.py
@@ -5,10 +5,12 @@ def get_pi(total=10000):
     in_circle = 0
     for i in range(total):
         pair = (random(), random())
-        sum = pair[0]**2 + pair[1]**2
-        if sum < 1:
+        squared_sum = pair[0]**2 + pair[1]**2
+        if squared_sum < 1:
             in_circle += 1
+        # in_circle += 1 if (random()**2+random()**2)<1 else in_circle
     return in_circle/total*4
 
 
-print(get_pi())
+if __name__ == __main__:
+    print(get_pi())
