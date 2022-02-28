0055d94a319b739ca1a1b0c3010da147bbf2684f
Egor <egorbgtrvch@gmail.com>
2022-02-28 19:17:27 +0300

    Some minor changes in context_timer.py

--- week3/context_timer.py
+++ week3/context_timer.py
@@ -2,17 +2,20 @@ from time import perf_counter
 
 
 class Timer:
+    def __init__(self):
+        self.total_time = 0.0
+
     def __enter__(self):
         self.start = perf_counter()
         self.end = 0.0
-        return lambda: self.end - self.start
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.end = perf_counter()
+        self.total_time = self.end - self.start
 
 
 if __name__ == '__main__':
-    with Timer() as timer:
+    my_timer = Timer()
+    with my_timer as timer:
         a = 9**8**7
-
-    print(timer())
+    print(my_timer.total_time)
