2713cceff9d74ec77b00259aaee6d2364c262f25
Egor <egorbgtrvch@gmail.com>
2022-03-01 13:43:36 +0300

    fixed mistakes

--- week3/valid_arguments.py
+++ week3/valid_arguments.py
@@ -1,5 +1,6 @@
 from typing import Callable
 
+
 class NotValidArguments(Exception):
     pass
 
@@ -9,17 +10,17 @@ def validator(func: Callable):
 
     def wrapper(*args):
         func_params = inspect.getfullargspec(func).annotations
-        i = 0
         wrong_args = ''
-        all_args = [*args]
-        for key, value in func_params.items():
-            if not isinstance(all_args[i], value):
+
+        for i, key in enumerate(func_params.keys()):
+            value = func_params[key]
+            if not isinstance(args[i], value):
                 param = f'"{key}" '
                 inform_str = f'----- The argument of the {func} parameter {param}' \
                              f'is of a wrong type:\n' \
                              f'          needed {value}, got {type(args[i])}\n'
                 wrong_args += inform_str
-            i += 1
+
         if wrong_args:
             wrong_args = '\n' + wrong_args
             raise NotValidArguments(wrong_args)
