17fcd4d48a0b8312279ff85ea84a18c85e86bca3
Egor <egorbgtrvch@gmail.com>
2022-03-02 22:31:35 +0300

    week4 initial

---
+++ week4/Point3D.py
@@ -0,0 +1,19 @@
+class Point3D:
+    def __init__(self, x: int = 0, y: int = 0, z: int = 0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __repr__(self):
+        return f'({self.x}, {self.y}, {self.z})'
+
+    # How can I annotate the type of argument "other"(Point3D instance)?
+    def distance(self, other: object) -> int:
+        dist = ((self.x-other.x)**2+(self.y-other.y)**2+(self.z-other.z)**2)**0.5
+        return dist
+
+
+if __name__ == '__main__':
+    my_point = Point3D()
+    point_2 = Point3D(0, 3, 4)
+    print(my_point.distance(point_2))
