92ddead0e05bdbf1bf035d12a18c2d972edd7552
Egor <egorbgtrvch@gmail.com>
2022-03-12 21:30:12 +0300

    added socket(task2)

---
+++ week5/socket/client.py
@@ -0,0 +1,28 @@
+import socket
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+host, port = os.environ.get('HOST_AND_PORT').split(':')
+HOST = str(host)
+PORT = int(port)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    expr = input('Enter the expression: ')
+    s.sendall(expr.encode(encoding='utf-8'))
+    if expr:
+        data = s.recv(1024).decode(encoding='utf-8')
+        res, _, error = data.partition(':::')
+        if res:
+            print(f"Received {res}")
+        elif error:
+            print(error)
+            print(res)
+        else:
+            print('Something unexpected happened. Please, connect with'
+                  'the support service.')
+            print(res)
+    else:
+        print('Session ended.')
---
+++ week5/socket/server.py
@@ -0,0 +1,45 @@
+import socket
+import os
+
+from dotenv import load_dotenv
+from operator import *
+
+load_dotenv()
+host, port = os.environ.get('HOST_AND_PORT').split(':')
+HOST = str(host)
+PORT = int(port)
+
+oper_dict = {'sum': add, 'div': truediv, 'diff': sub,
+             'mult': mul, 'expon': pow, 'floordiv': floordiv,
+             'and': and_, 'xor': xor, 'or': or_,
+             'is': is_, 'is_not': is_not, 'lshift': lshift,
+             'mod': mod, 'rshift': rshift, 'lt': lt, 'le': le,
+             'eq': eq, 'ne': ne, 'gt': gt, 'ge': ge}
+
+
+def calculate(expression: str):
+    try:
+        oper, val1, val2 = expression.strip().split(' ')
+        res = oper_dict[oper](float(val1), float(val2))
+        return str(res) + ':::'
+    except ZeroDivisionError:
+        return ':::You are trying to divide by zero.'
+    except KeyError:
+        return ':::There is no such operation.'
+    except ValueError:
+        return ':::Invalid arguments or their types.'
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        with conn:
+            print(f"Connected by {addr}")
+            data = conn.recv(1024).decode(encoding='utf-8')
+            if not data:
+                break
+            send_data = calculate(data).encode(encoding='utf-8')
+            conn.sendall(send_data)
+
