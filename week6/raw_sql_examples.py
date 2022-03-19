309b4b04d32d9399c7eeefc49abf706c3f484091
Egor <egorbgtrvch@gmail.com>
2022-03-19 20:47:16 +0300

    Added raw_sql_examples.py

---
+++ week6/raw_sql_examples.py
@@ -0,0 +1,24 @@
+import psycopg2
+
+from instinctools_GA.week5.config import settings
+
+
+DATABASE = settings.DATABASE
+conn = psycopg2.connect(**DATABASE)
+cur = conn.cursor()
+
+# 1
+cur.execute("SELECT * FROM operations WHERE result<%(res)s", {'res': 0})
+
+# 2
+cur.execute("SELECT COUNT(*) FROM operations WHERE result<%s", (0, ))
+
+# 3
+cur.execute('''SELECT operator, COUNT(operator)
+FROM operations
+GROUP BY operator''')
+
+# 4
+cur.execute('''SELECT operator, CAST(COUNT(operator)/(SELECT COUNT(*) FROM operations) AS INT)
+FROM operations
+GROUP BY operator''')
