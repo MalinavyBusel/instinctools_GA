f838a7d6e638f265ddb97a2b5560b054d8ff42fd
Egor <egorbgtrvch@gmail.com>
2022-04-11 15:49:47 +0300

    sortings tests simplified

--- week9/json_sorter/tests/test_sortings.py
+++ week9/json_sorter/tests/test_sortings.py
@@ -11,49 +11,16 @@ sorted_2 = [-10, -1, 0, 1, 3, 4, 5, 6, 7, 11, 34, 51, 87]
 sorted_3 = [-345, -23.56, 0, 7.1, 8, 10, 12.84, 19.001, 74, 893, 5823]
 
 
-def test_insertion_1():
-    assert insertion_sort(unsorted_1) == sorted_1
+def test_1():
+    assert insertion_sort(unsorted_1) == merge_sort(unsorted_1) \
+           == quick_sort(unsorted_1) == shaking_sort(unsorted_1)
 
 
-def test_insertion_2():
-    assert insertion_sort(unsorted_2) == sorted_2
+def test_2():
+    assert insertion_sort(unsorted_2) == merge_sort(unsorted_2) \
+           == quick_sort(unsorted_2) == shaking_sort(unsorted_2)
 
 
-def test_insertion_3():
-    assert insertion_sort(unsorted_3) == sorted_3
-
-
-def test_merge_1():
-    assert merge_sort(unsorted_1) == sorted_1
-
-
-def test_merge_2():
-    assert merge_sort(unsorted_2) == sorted_2
-
-
-def test_merge_3():
-    assert merge_sort(unsorted_3) == sorted_3
-
-
-def test_quick_sort_1():
-    assert quick_sort(unsorted_1) == sorted_1
-
-
-def test_quick_sort_2():
-    assert quick_sort(unsorted_2) == sorted_2
-
-
-def test_quick_sort_3():
-    assert quick_sort(unsorted_3) == sorted_3
-
-
-def test_shaking_1():
-    assert shaking_sort(unsorted_1) == sorted_1
-
-
-def test_shaking_2():
-    assert shaking_sort(unsorted_2) == sorted_2
-
-
-def test_shaking_3():
-    assert shaking_sort(unsorted_3) == sorted_3
+def test_3():
+    assert insertion_sort(unsorted_3) == merge_sort(unsorted_3) \
+           == quick_sort(unsorted_3) == shaking_sort(unsorted_3)
